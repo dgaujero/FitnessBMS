f71441416f8055a3a8a2013e2fc06c9c15296730
dgaujero <deion@aujero.net>
2020-02-18 19:04:02 -0500

    added member portal js

--- config/orm.js
+++ config/orm.js
@@ -27,3 +27,24 @@ function objToSql(ob) {
 
     return arr.toString();
 }
+
+var orm = {
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+        console.log(queryString);
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      }
+}
+
+module.exports = orm;
